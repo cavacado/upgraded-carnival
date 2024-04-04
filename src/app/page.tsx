074efa65cb36aa2e94c90b68dd03d06981a9031e
zl <ng_zhao_loong@tech.gov.sg>
2024-04-04 15:46:06 +0800

    chore: add quick links for analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      Hello World.
+      <nav>
+        <Link href="/about">About</Link>
+        <br />
+        <Link href="/form">Form</Link>
+        <br />
+        <Link href="/file-download">Download</Link>
+        <br />
+        <Link href="/embed">Embed</Link>
+        <br />
+        <Link href="/outbound">Outbound</Link>
+      </nav>
     </div>
   );
 }
