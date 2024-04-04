1554793d3038c997609fd52aa0d6d4797878e255
zl <ng_zhao_loong@tech.gov.sg>
2024-04-04 16:32:34 +0800

    chore: add link label

--- src/app/outbound/page.tsx
+++ src/app/outbound/page.tsx
@@ -1,4 +1,4 @@
 import Link from "next/link";
 export default function Outbound() {
-  return <Link href={"https://www.channelnewsasia.com/"}></Link>;
+  return <Link href={"https://www.channelnewsasia.com/"}>Link to outbound</Link>;
 }
