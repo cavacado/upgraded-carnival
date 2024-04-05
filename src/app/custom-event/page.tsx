46bdf248e1369fa09089ec0fd17a6a270a9129a3
zl <ng_zhao_loong@tech.gov.sg>
2024-04-05 10:13:23 +0800

    feat: add custom event tracking

---
+++ src/app/custom-event/page.tsx
@@ -0,0 +1,18 @@
+import { sendGAEvent } from "@next/third-parties/google";
+export default function CustomEvent() {
+  return (
+    <>
+      <h1>Showcasing custom event:</h1>
+      <br />
+      <button
+        onClick={() => {
+          sendGAEvent("event", "custom_event", {
+            value: "coolcoolcool",
+          });
+        }}
+      >
+        send custom event
+      </button>
+    </>
+  );
+}
