0ae79603eb86f12360690e1ffd28c9ca2d7c957c
zl <ng_zhao_loong@tech.gov.sg>
2024-04-15 13:55:59 +0800

    fix: file_download events

--- src/app/file-download/page.tsx
+++ src/app/file-download/page.tsx
@@ -2,7 +2,7 @@ export default function FileDownload() {
   return (
     <>
       <h1>Showcasing file download:</h1>
-      <a href={"https://pastebin.pl/view/download/8b17e463"}>
+      <a href={"https://filesamples.com/samples/document/txt/sample1.txt"} download>
         Download
       </a>
     </>
