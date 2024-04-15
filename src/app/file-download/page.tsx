d15563b526b55f7970e25f014562ecda466bb3bd
zl <ng_zhao_loong@tech.gov.sg>
2024-04-15 13:48:38 +0800

    chore: test file download

--- src/app/file-download/page.tsx
+++ src/app/file-download/page.tsx
@@ -2,7 +2,7 @@ export default function FileDownload() {
   return (
     <>
       <h1>Showcasing file download:</h1>
-      <a href={"https://pastebin.pl/view/download/8b17e463"} download>
+      <a href={"https://pastebin.pl/view/download/8b17e463"}>
         Download
       </a>
     </>
